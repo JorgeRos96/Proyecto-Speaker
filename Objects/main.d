c46299e3dcdd8da08fc1fc6af1da8d32195ae1bb
JorgeRos96 <86102864+JorgeRos96@users.noreply.github.com>
2021-07-26 17:39:03 +0200

    Add files via upload

--- Objects/main.d
+++ Objects/main.d
@@ -29,3 +29,5 @@
 .\objects\main.o: C:\Users\User\AppData\Local\Arm\Packs\Keil\STM32F4xx_DFP\2.15.0\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_pwr_ex.h
 .\objects\main.o: C:\Users\User\AppData\Local\Arm\Packs\Keil\STM32F4xx_DFP\2.15.0\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_tim.h
 .\objects\main.o: C:\Users\User\AppData\Local\Arm\Packs\Keil\STM32F4xx_DFP\2.15.0\Drivers\STM32F4xx_HAL_Driver\Inc\stm32f4xx_hal_tim_ex.h
+.\objects\main.o: speaker.h
+.\objects\main.o: Delay.h
